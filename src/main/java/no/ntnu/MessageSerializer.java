199ac37c2bb91f66aabe1b28058687a50d972760
runemola <runemola@stud.ntnu.no>
2023-12-08 20:16:22 +0100

    Added support for more messages and the command to toggle an actuator

--- src/main/java/no/ntnu/MessageSerializer.java
+++ src/main/java/no/ntnu/MessageSerializer.java
@@ -1,18 +1,24 @@
 package no.ntnu;
 
-import no.ntnu.command.GetTemperatureCommand;
+import no.ntnu.command.ToggleActuatorCommand;
 import no.ntnu.message.HumidityMessage;
+import no.ntnu.message.TemperatureMessage;
 
 public class MessageSerializer {
     private final static String humidityUnit = "%";
     private final static String temperatureUnit = "C";
+    private final static String actuatorCommand = "A";
     
     private MessageSerializer() {}
 
     public static String toString(Message message) {
         String m = "";
-        if (message instanceof GetTemperatureCommand) {
-            m = "";
+        if (message instanceof HumidityMessage humidityMessage) {
+            m = humidityMessage.getHumidity() + "%," + humidityMessage.getNodeId();
+        } else if (message instanceof ToggleActuatorCommand toggleActuatorCommand) {
+            m = actuatorCommand + toggleActuatorCommand.getNodeId() + "," + toggleActuatorCommand.getId();
+        } else if (message instanceof TemperatureMessage temperatureMessage) {
+            m = temperatureMessage.getTemperature() + "C," + temperatureMessage.getNodeId();
         }
 
         return m;
@@ -24,8 +30,13 @@ public class MessageSerializer {
         if (isUnitValid(s)) {
             switch (getUnit(s)) {
                 case '%' :
-                    m = new HumidityMessage(0);
+                    m = new HumidityMessage(parseDouble(s,0), parseInteger(s, 1));
+                    break;
+                case 'C':
+                    m = new TemperatureMessage(parseDouble(s, 0), parseInteger(s, 1));
             } 
+        } else if (s.startsWith(actuatorCommand)) {
+            m = new ToggleActuatorCommand(parseInteger(s, 0), parseInteger(s, 1));
         }
 
         return m;
@@ -33,7 +44,8 @@ public class MessageSerializer {
 
     private static boolean isUnitValid(String message) {
         boolean valid = false;
-        char last = message.charAt(message.length()-1);
+        String[] split = message.split(",");
+        char last = split[0].charAt(message.length()-1);
         if(last == humidityUnit.charAt(0) || last == temperatureUnit.charAt(0)) valid = true;
 
         return valid;
@@ -49,13 +61,44 @@ public class MessageSerializer {
         return c;
     }
 
-    public static double parseDouble(String s) {
+    public static double parseDouble(String s, int position) {
+        String[] split = s.substring(0, s.length()-1).split(",");
         double value = 0;
         try {
-            value = Double.valueOf(s.substring(0, s.length()-1));
+            value = Double.valueOf(split[position]);
         } catch (NumberFormatException e) {
             System.err.println("Could not parse String <" + s + "> to double");
         }
         return value;
+    }
+
+    private static Integer parseInteger(String s, int position) {
+        Integer i = null;
+        String[] split;
+        if (isUnitValid(s)) {
+            split = s.substring(0, s.length()-1).split(",");
+        } else {
+            split = s.substring(1).split(",");
+        }
+        try {
+            Integer.valueOf(split[position]);
+        } catch (Exception e) {
+            System.err.println("Could not parse integer <" + s + ">");
+        }
+
+        return i;
+    }
+
+    public static Message ParseParameterizedMessage(String s) {
+        Message m = null;
+
+        String parameter = s.substring(1);
+        if (s.startsWith(actuatorCommand)) {
+            Integer nodeId = parseInteger(parameter, 0);
+            Integer actuatorId = parseInteger(parameter, 1);
+            m = new ToggleActuatorCommand(nodeId, actuatorId);
+        }
+
+        return m;
     }
 }
