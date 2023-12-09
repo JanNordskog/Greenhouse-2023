52dca7fbae213673d6a16eb390a0c367ec9b7871
UlrikRibler <89103076+UlrikRibler@users.noreply.github.com>
2023-12-09 20:38:23 +0100

    Added CommunicationChannel test class on runes branch from master branch

--- src/test/controlpanelTest/CommunicationChannelTest.java
+++ src/test/controlpanelTest/CommunicationChannelTest.java
@@ -1,4 +1,39 @@
-package controlpanelTest;
+package no.ntnu.controlpanel;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CommunicationChannelTest {
+
+  @Test
+  public void testSendActuatorChange() {
+    // Create an instance of a class that implements CommunicationChannel (replace with your implementation)
+    CommunicationChannel channel = new YourCommunicationChannelImplementation();
+
+    // Define test parameters
+    int nodeId = 1;
+    int actuatorId = 2;
+    boolean isOn = true;
+
+    // Call the sendActuatorChange method
+    channel.sendActuatorChange(nodeId, actuatorId, isOn);
+
+    // Add assertions to check if the method behaves as expected
+    // For example, you can assert that certain actions were performed or values were updated.
+    // Example assertions:
+    // assertTrue(channel.isActuatorStateChanged()); // Check if the state changed
+    // assertEquals(nodeId, channel.getLastNodeId()); // Check if the node ID matches
+  }
+
+  @Test
+  public void testOpen() {
+    // Create an instance of a class that implements CommunicationChannel (replace with your implementation)
+    CommunicationChannel channel = new YourCommunicationChannelImplementation();
+
+    // Call the open method
+    boolean isOpen = channel.open();
+
+    // Add assertions to check if the method behaves as expected
+    assertTrue(isOpen); // Check if the channel is successfully opened
+  }
 }
