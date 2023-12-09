1cd891592ec66c8eca7befd5599c977dd2ce642e
UlrikRibler <89103076+UlrikRibler@users.noreply.github.com>
2023-12-09 23:09:03 +0100

    GreenhouseSimulator test class

--- src/test/java/no/ntnu/greenhouseTest/GreenhouseSimulatorTest.java
+++ src/test/java/no/ntnu/greenhouseTest/GreenhouseSimulatorTest.java
@@ -1,4 +1,24 @@
-package greenhouseTest;
+package no.ntnu.greenhouse;
+
+import no.ntnu.Message;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GreenhouseSimulatorTest {
+  private GreenhouseSimulator simulator;
+
+  @BeforeEach
+  void setUp() {
+    // Create a greenhouse simulator with the "fake" mode for testing
+    simulator = new GreenhouseSimulator(true);
+  }
+
+  @Test
+  void testInitialize() {
+    simulator.initialize();
+    assertNotNull(simulator, "Simulator should not be null");
+  }
+
+
 }
