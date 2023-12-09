da247d952fab07165b606aa089a7534504c079ce
Jan Nordskog <jan.nordskog@gmail.com>
2023-12-09 22:31:30 +0100

    Fixed actuatorcollectiontest and started on readmefile

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Greenhouse-2023
+
+## Code contributers
+This project exists thanks to all the people who contribute.
+
+👤Rune molander
+
+👤Ulrik Ribler
+
+👤Rune molander
+
+👤Jan Nordkosg
+
+---
+## Author
+👤Vegard Mything
+
+👤Jan Nordkosg
--- src/test/java/no/ntnu/greenhouseTest/ActuatorCollectionTest.java
+++ src/test/java/no/ntnu/greenhouseTest/ActuatorCollectionTest.java
@@ -1,74 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package no.ntnu.greenhouse;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import no.ntnu.greenhouse.Actuator;
-import no.ntnu.greenhouse.ActuatorCollection;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ActuatorCollectionTest {
-
   private ActuatorCollection collection;
-  private Actuator actuator1;
-  private Actuator actuator2;
 
   @BeforeEach
-  public void setUp() {
-    // Create an ActuatorCollection instance for testing
+  void setUp() {
     collection = new ActuatorCollection();
-
-    // Create Actuator instances for testing
-    actuator1 = new Actuator("Heater", 1);
-    actuator2 = new Actuator("Fan", 2);
-
-    // Add actuators to the collection
-    collection.add(actuator1);
-    collection.add(actuator2);
   }
 
   @Test
-  public void testAddAndGet() {
-    // Check if the actuators were added to the collection
-    assertEquals(2, collection.size());
-
-    // Check if we can retrieve an actuator by its ID
-    Actuator retrievedActuator1 = collection.get(actuator1.getId());
-    Actuator retrievedActuator2 = collection.get(actuator2.getId());
-
-    assertNotNull(retrievedActuator1);
-    assertNotNull(retrievedActuator2);
-
-    // Check if the retrieved actuators match the original actuators
-    assertEquals(actuator1, retrievedActuator1);
-    assertEquals(actuator2, retrievedActuator2);
+  void testAddAndGetActuator() {
+    Actuator actuator = new Actuator("type 1", 1);
+    collection.add(actuator);
+    assertEquals(actuator, collection.get(1), "Actuator should be retrieved correctly by ID");
   }
 
-  @Test
-  public void testIterator() {
-    // Check if the iterator returns all actuators in the collection
-    int count = 0;
-    for (Actuator actuator : collection) {
-      if (actuator == actuator1 || actuator == actuator2) {
-        count++;
-      }
-    }
-    assertEquals(2, count);
-  }
 
-  @Test
-  public void testSize() {
-    // Check if the size method returns the correct number of actuators
-    assertEquals(2, collection.size());
-
-    // Add another actuator to the collection
-    Actuator actuator3 = new Actuator("Light", 3);
-    collection.add(actuator3);
-
-    // Check if the size method updates correctly
-    assertEquals(3, collection.size());
-  }
-
-  @Test
-  public void testDebugPrint() {
-    // Check if debugPrint() method prints actuator info without errors
-    collection.debugPrint();
-  }
 }
