9348415e8dac2ff2494acbdd8de72bfcd80ec0a6
avnit-1302 <122270703+avnit-1302@users.noreply.github.com>
2023-12-10 17:10:00 +0100

    test for ActuatorMessage

--- src/test/java/no/ntnu/messageTest/ActuatorMessageTest.java
+++ src/test/java/no/ntnu/messageTest/ActuatorMessageTest.java
@@ -1,4 +1,22 @@
-package messageTest;
+package no.ntnu.messageTest;
+
+import no.ntnu.message.ActuatorMessage;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ActuatorMessageTest {
+
+  @Test
+  public void testActuatorMessageCreation() {
+    int nodeId = 1;
+    int actuatorId = 2;
+    boolean isOn = true;
+
+    ActuatorMessage message = new ActuatorMessage(nodeId, actuatorId, isOn);
+
+    // Testing getters
+    assertEquals(nodeId, message.getNodeId(), "Node ID should match the constructor argument");
+    assertEquals(actuatorId, message.getActuatorId(), "Actuator ID should match the constructor argument");
+    assertEquals(isOn, message.isOn(), "Actuator state should match the constructor argument");
+  }
 }
