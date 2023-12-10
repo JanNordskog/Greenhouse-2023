f923ddfef89f4444e3c44701da43c7a115c9363c
UlrikRibler <89103076+UlrikRibler@users.noreply.github.com>
2023-12-10 16:22:55 +0100

    ActuatorMessage test class

--- src/test/java/no/ntnu/messageTest/ActuatorMessageTest.java
+++ src/test/java/no/ntnu/messageTest/ActuatorMessageTest.java
@@ -1,4 +1,18 @@
-package messageTest;
+package no.ntnu.message;
+
+import no.ntnu.MessageSerializer;
 
 public class ActuatorMessageTest {
+
+  public static void main(String[] args) {
+    testActuatorMessageSerialization();
+  }
+
+  private static void testActuatorMessageSerialization() {
+    ActuatorMessage actuatorMessage = new ActuatorMessage(1, 2, true);
+    String serialized = MessageSerializer.toString(actuatorMessage);
+    System.out.println("Serialized ActuatorMessage: " + serialized);
+  }
+
+  // You can add more methods if you need to test different scenarios
 }
