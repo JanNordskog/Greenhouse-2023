59991a7d62e91331c8a3747bede8068cb72f67c4
avnit-1302 <122270703+avnit-1302@users.noreply.github.com>
2023-12-10 17:29:46 +0100

    test for parser

--- src/test/java/no/ntnu/toolsTest/ParserTest.java
+++ src/test/java/no/ntnu/toolsTest/ParserTest.java
@@ -1,4 +1,36 @@
-package toolsTest;
+package no.ntnu.toolsTest;
+
+
+import no.ntnu.tools.Parser;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
+
+  @Test
+  public void testParseIntegerOrErrorWithValidInput() {
+    assertEquals(123, Parser.parseIntegerOrError("123", "Error parsing integer"));
+  }
+
+  @Test
+  public void testParseIntegerOrErrorWithInvalidInput() {
+    String errorMessage = "Error parsing integer";
+    assertThrows(NumberFormatException.class, () -> {
+      Parser.parseIntegerOrError("abc", errorMessage);
+    }, errorMessage);
+  }
+
+  @Test
+  public void testParseDoubleOrErrorWithValidInput() {
+    assertEquals(123.45, Parser.parseDoubleOrError("123.45", "Error parsing double"));
+  }
+
+  @Test
+  public void testParseDoubleOrErrorWithInvalidInput() {
+    String errorMessage = "Error parsing double";
+    assertThrows(NumberFormatException.class, () -> {
+      Parser.parseDoubleOrError("abc", errorMessage);
+    }, errorMessage);
+  }
 }
+
