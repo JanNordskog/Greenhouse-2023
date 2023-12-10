1aaa696bb8ca0f084e08b097039fb7b59ff5b453
UlrikRibler <89103076+UlrikRibler@users.noreply.github.com>
2023-12-10 01:34:06 +0100

    SensorActuator test class

---
+++ src/test/java/no/ntnu/controlpanelTest/SensorActuatorNodeInfoTest.java
@@ -0,0 +1,29 @@
+package no.ntnu.controlpanel;
+
+import no.ntnu.greenhouse.Actuator; // Import statement for Actuator class
+
+public class SensorActuatorNodeInfoTest {
+
+  public static void main(String[] args) {
+    SensorActuatorNodeInfo nodeInfo = new SensorActuatorNodeInfo(1);
+    System.out.println("Testing SensorActuatorNodeInfo with nodeId = 1");
+
+    // Test: Get ID of the node
+    if (nodeInfo.getId() == 1) {
+      System.out.println("getId() passed.");
+    } else {
+      System.out.println("getId() failed.");
+    }
+
+    // Test: Add and get an actuator
+    Actuator actuator = new Actuator("testType", 2);
+    nodeInfo.addActuator(actuator);
+
+    Actuator retrievedActuator = nodeInfo.getActuator(2);
+    if (retrievedActuator != null && retrievedActuator.equals(actuator)) {
+      System.out.println("addActuator() and getActuator() passed.");
+    } else {
+      System.out.println("addActuator() and getActuator() failed.");
+    }
+  }
+}
